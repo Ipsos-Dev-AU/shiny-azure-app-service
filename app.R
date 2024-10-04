241c35202b2b50295dee85f2f4d7150047d29b19
Shaikh-Sho <shoaib.shaikh@ipsos.com>
2024-10-04 16:26:46 +0000

    Added feature to connect to sql database to test from Azure

--- app.R
+++ app.R
@@ -1,10 +1,117 @@
 library(shiny)
+library(DBI)
+library(shinyjs)
 
 # Define a list of words for the game
 words <- c("hangman", "programming", "shiny", "authentication", "azure", "container")
 
-ui <- fluidPage(
+# This function creates a connection engine using sys env creds -----------
+conn <- function(.driver = driver,
+                 .server = server,
+                 .database = database,
+                 .uid = uid,
+                 .pwd = pwd ,
+                 .port = port,
+                 .trusted_connection = trusted_connection){
+  DBI::dbConnect(odbc::odbc(),
+                 driver = .driver,
+                 server = .server,
+                 database = .database,
+                 uid = .uid,
+                 pwd = .pwd,
+                 port = .port,
+                 trusted_connection = .trusted_connection)
+}
+
+# This function test manual connection to server ---------------------------------
+test_conn <- function(driver,
+                      server,
+                      database,
+                      port,
+                      trusted_connection,
+                      uid,
+                      pwd) {
+  .GlobalEnv$`admin_conn` <-
+    conn(
+      .driver = driver,
+      .server = server,
+      .database = database,
+      .port = port,
+      .trusted_connection = trusted_connection,
+      .uid = uid,
+      .pwd = pwd
+    )
   
+  # result <-
+  #   dbIsValid(.GlobalEnv$`admin_conn`)
+  
+  # return(result)
+}
+# This function creates a form for server specs -------------
+server_conn_form <- function(.driver = driver,
+                             .server = server,
+                             .database = database,
+                             .port = port,
+                             .trusted_connection = trusted_connection,
+                             .uid = uid,
+                             .pwd = pwd)
+{
+  shiny::tagList(
+    shiny::tags$h3("Server Specs"),
+    textInput(
+      inputId = "driver",
+      label = "Driver",
+      value = .driver,
+      placeholder = "Enter conn driver name"
+    ),
+    textInput(
+      inputId = "server",
+      label = "Server",
+      value = .server,
+      placeholder = "Enter conn server name"
+    ),
+    textInput(
+      inputId = "database",
+      label = "Database",
+      value = .database,
+      placeholder = "Enter conn database name"
+    ),
+    textInput(
+      inputId = "port",
+      label = "Port",
+      value = .port,
+      placeholder = "Enter conn port name"
+    ),
+    selectInput(
+      inputId = "trusted_connection",
+      label = "Trusted Connection",
+      choices = c("", "true", "false"),
+      selected = .trusted_connection 
+    ),
+    textInput(
+      inputId = "uid",
+      label = "UID",
+      value = NULL,
+      placeholder = "Enter conn UID"
+    ),
+    passwordInput(
+      inputId = "pwd",
+      label = "PWD",
+      value = NULL,
+      placeholder = "Enter conn PWD"
+    ),
+    selectInput(
+      inputId = "connection_timeout",
+      label = "Connection Timeout", 
+      choices = list("1 Minute" = 60000, "5 Minutes" = 300000, "10 Minutes" = 600000),
+      selected = list("10 Minutes" = 600000)
+    )
+  )
+}
+
+# UI ------------
+ui <- fluidPage(
+  useShinyjs(),
   tags$head(
     tags$script(src = "auth.js")
   ),
@@ -44,10 +151,13 @@ ui <- fluidPage(
     textOutput("remaining_chances"),
     br(),
     tagAppendAttributes(style = "background-color:orange;color:white;",
-                        actionButton("reset_button", icon = icon("redo"), "Reset"))
+                        actionButton("reset_button", icon = icon("redo"), "Reset")),
+    hr(),
+    actionButton("db_conn", icon = icon("database"), "Connect")
   )
 )
 
+# Server -------------
 server <- function(input, output, session) {
   
   observe({
@@ -60,6 +170,101 @@ server <- function(input, output, session) {
       showNotification(paste0("Hey ", input$AzureAuth$name, " 👋"), duration = 5, type = "message")
     }
     
+  })
+  
+  observeEvent(input$db_conn, {
+    showModal(
+      modalDialog(
+        title = "Connect to a db",
+        server_conn_form(
+          .driver = NULL,
+          .server = NULL,
+          .database = NULL,
+          .port = NULL,
+          .trusted_connection = NULL,
+          .uid = NULL,
+          .pwd = NULL
+        ),
+        uiOutput("qry_text"),
+        verbatimTextOutput("qry_result_table"),
+        footer = tagList(
+          actionButton("testAdminConn", paste("Test Connection"), class = "btn-success"),
+          modalButton('Close')
+        )
+      )
+    )
+  })
+  
+  # observeEvent - input$testAdminConn --------------------------------------
+  observeEvent(input$testAdminConn, {
+    print("Testing connection", col = 31)
+    print(input$connection_timeout)
+    tryCatch(
+      expr = 
+        {
+          test <- test_conn(
+            driver = input$driver,
+            server = input$server,
+            database = input$database,
+            port = input$port,
+            trusted_connection = input$trusted_connection,
+            uid = input$uid,
+            pwd = input$pwd
+          )
+          print(paste0("Connected to ",input$server,".[", input$database, "]"))
+          showNotification(paste0("Connected to ",input$server,".[", input$database, "]"), duration = 5, type = "message")
+          
+        },
+      error = function(e) {
+        warning(e$message)
+        showNotification(e$message, duration = 5, type = "error")
+      })
+    
+    # Render UIInputs Table ---------------------------------------------------
+    if (exists("admin_conn")) {
+      # req(dbIsValid(admin_conn))
+      # reactive_data$UIInputs_data <- get_data("UIInputs", where = " 1=1 ORDER BY Id DESC", mydb = admin_conn, disconnect = FALSE)
+      
+      # Render UIInputs_data
+      output$qry_text <-
+        renderUI(
+          tagList(
+            textAreaInput(
+              inputId = "qry",
+              label = "Query"
+            ),
+            actionButton(
+              inputId = "run_qry",
+              label = NULL,
+              icon = icon("play")
+            )
+          )
+        )
+      
+      observeEvent(input$run_qry,{
+        print(input$qry)
+        qry_result <- DBI::dbGetQuery(admin_conn, input$qry)
+        print(qry_result)
+        
+        output$qry_result_table <-
+          renderPrint(qry_result)
+      })
+      
+    } else {
+      expr = 
+        showNotification("No valid connection exisits to the database.", duration = 5, type = "error")
+    }
+    
+    print(paste("Checking if `admin_conn` is valid and disconnecting in", as.numeric(input$connection_timeout)/1000/60 ,"minutes if it does"))
+    shinyjs::delay(
+      as.numeric(input$connection_timeout),
+      if (dbIsValid(admin_conn)) {
+        dbDisconnect(admin_conn)
+        print("Auto Timeout..Disconnected from database")
+        expr = 
+          showNotification("Disconnected from databse.", duration = 5, type = "message")
+      }
+    )
   })
   
   # Initialize game state
